91d89b4d4191493c0156555d31e08eddfc61729d
Temur <tim_1998@bk.ru>
2020-03-10 21:33:22 +0300

    Debug

--- databases/gluster.py
+++ databases/gluster.py
@@ -59,8 +59,6 @@ def get_word_inverted_index(word: str) -> set:
     if volume.exists(path):
         with volume.fopen(path, 'r') as f:
             file = f.read()
-            print(file)
-            print(file.decode(), 'orgboreibg')
             decoded_nums = custom_decode(file)
             iindex = iindex.union(set(decoded_nums))
     path = os.path.join(AUXILIARY_IINDEX_DIR_PATH, word)
