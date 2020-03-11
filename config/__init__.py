83a37c9b1d0074ad4cd205a0c9f17fa29766f27e
Temur <tim_1998@bk.ru>
2020-03-11 20:28:28 +0300

    Fix removing of directories

--- config/__init__.py
+++ config/__init__.py
@@ -2,6 +2,7 @@ DOCUMENTS_DIR_PATH = '/mnt/gfsvol/documents/'
 MAIN_IINDEX_DIR_PATH = '/mnt/gfsvol/main-inverted-index/'
 AUXILIARY_IINDEX_DIR_PATH = '/mnt/gfsvol/auxiliary-inverted-index/'
 REMOVABLE_IINDEX_DIR_PATH = '/mnt/gfsvol/removable-inverted-index/'
+VOLUME_DIR_PATH = '/mnt/gfsvol/'
 
 GLUSTERFS_MASTER_IP_ADDRESS = '127.0.0.1'
 ENGINE_DATE_VOLUME = 'engine-data-volume'
